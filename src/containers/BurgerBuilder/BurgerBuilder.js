8d652505013e27e1fcc45b0d9f077e8e78e877b0
walosha <walosha@yahoo.com>
2019-09-13 10:22:45 +0100

    changes staged

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-import Aux from "../../hoc/aux";
+import Aux from "../../hoc/Aux";
 import Burger from "../../components/Burger/Burger";
 
 class BurgerBuilder extends Component {
