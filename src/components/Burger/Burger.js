c1835a2e8dc3b179fbe66ea3fad21436c56ba860
walosha <walosha@yahoo.com>
2019-09-13 18:03:37 +0100

    updated the burgetControl

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,28 +1,31 @@
-import React from 'react';
+import React from "react";
 
-import classes from './Burger.css';
-import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+import classes from "./Burger.css";
+import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
-const burger = ( props ) => {
-    let transformedIngredients = Object.keys( props.ingredients )
-        .map( igKey => {
-            return [...Array( props.ingredients[igKey] )].map( ( _, i ) => {
-                return <BurgerIngredient key={igKey + i} type={igKey} />;
-            } );
-        } )
-        .reduce((arr, el) => {
-            return arr.concat(el)
-        }, []);
-    if (transformedIngredients.length === 0) {
-        transformedIngredients = <p>Please start adding ingredients!</p>;
-    }
-    return (
-        <div className={classes.Burger}>
-            <BurgerIngredient type="bread-top" />
-            {transformedIngredients}
-            <BurgerIngredient type="bread-bottom" />
-        </div>
-    );
+const burger = props => {
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(ingredient => {
+      return [...Array(props.ingredients[ingredient])].map((_, ikey) => {
+        return (
+          <BurgerIngredient key={ikey} type={ingredient}></BurgerIngredient>
+        );
+      });
+    })
+    .reduce((prevArray, ele) => {
+      return [...prevArray, ele];
+    });
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>;
+  }
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type="bread-top" />
+      {transformedIngredients}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
 };
 
 export default burger;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,26 +2,44 @@ import React, { Component } from "react";
 
 import Aux from "../../hoc/Aux";
 import Burger from "../../components/Burger/Burger";
+import BuildControls from "../../components/BuildControls/BuildControls";
+
+const INGREDIENT_PRICES = {
+  salad: 200,
+  bacon: 150,
+  cheese: 250,
+  meat: 400
+};
 
 class BurgerBuilder extends Component {
-  // constructor(props) {
-  //     super(props);
-  //     this.state = {...}
-  // }
   state = {
     ingredients: {
       salad: 0,
       bacon: 0,
       cheese: 0,
       meat: 0
-    }
+    },
+    totalPrice: 100,
+    disabled: false
+  };
+
+  removeIngredientHandler() {}
+
+  addIngredientHandler = type => {
+    const quantity = this.state.ingredients[type] + 1;
+    const price = INGREDIENT_PRICES[type] * quantity;
+    const updatedPrice = this.state.totalPrice + price;
+    this.setState({ ingredients: quantity, totalPrice: updatedPrice });
   };
 
   render() {
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients} />
-        <div>Build Controls</div>
+        <BuildControls
+          added={this.addIngredientHandler}
+          price={this.state.totalPrice}
+        ></BuildControls>
       </Aux>
     );
   }
